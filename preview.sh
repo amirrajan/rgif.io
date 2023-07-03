aa41f0d6173ba5562ce42c2dd87ce81a46e07869
Amir Rajan <ar@amirrajan.net>
2023-07-03 13:43:31 -0500

    Added dissapointed fan and dissapointed justin timberlake.

--- preview.sh
+++ preview.sh
@@ -2,27 +2,9 @@
 
 # Get the current directory
 current_dir=$(pwd)
-
 set +x
-# Loop through each GIF file in the current directory
-for gif_file in "$current_dir"/*.gif; do
-    # Extract the filename and extension
-    filename=$(basename "$gif_file")
-    extension="${filename##*.}"
-    filename="${filename%.*}"
-
-    # Get the total number of frames
-    total_frames=$(identify -format "%n\n" $gif_file | head -1)
-
-    # Calculate the middle frame index
-    middle_frame_index=$((total_frames / 2))
-    middle_frame_index=${middle_frame_index%.*}
-
-    echo $total_frames
-    echo $middle_frame_index
-
-    # Extract the middle frame using ImageMagick's convert command
-    convert "$gif_file[$middle_frame_index]" "$filename.png"
-
-    echo "Extracted middle frame from '$gif_file' and saved as '$output_filename'"
-done
+gif_file="$current_dir/disappointed-fan.gif"
+filename=$(basename "$gif_file")
+filename="${filename%.*}"
+convert "$gif_file[0]" "$filename.png"
+echo "Extracted 0 frame from '$gif_file' and saved as $filename.png"
