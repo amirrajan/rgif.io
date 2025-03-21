90ea79c629b247d175bbc447155ce91b589d6746
Amir Rajan <ar@amirrajan.net>
2025-03-21 12:15:28 -0500

    added galdiator entertained meme.

--- preview.sh
+++ preview.sh
@@ -3,8 +3,8 @@
 # Get the current directory
 current_dir=$(pwd)
 set +x
-file="$current_dir/surprised-pikachu.gif"
+file="$current_dir/entertained.gif"
 filename=$(basename "$file")
 filename="${filename%.*}"
-convert "$file[0]" "$filename.png"
+magick "$file[0]" "$filename.png"
 echo "Extracted 0 frame from '$file' and saved as $filename.png"
