7d78cf5d442a207db9bdcef31e53ec36a3b93d4b
Amir Rajan <ar@amirrajan.net>
2025-03-21 21:25:17 -0500

    delete stupid .DS_Store directory.

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
-rm *~*
+find . -name ".DS_Store" -type f -delete
 aws s3 sync . s3://rgif.io --delete
 aws cloudfront create-invalidation --distribution-id $(cat .distribution_id) --paths /index.html
