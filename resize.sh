f0aec0c0063897b895ae7264ec75bfa4295cfd5d
Amir Rajan <ar@amirrajan.net>
2025-08-12 20:26:56 -0500

    Added jordan stop-it.gif

--- resize.sh
+++ resize.sh
@@ -7,7 +7,7 @@ directory="."
 current_dir=$(pwd)
 set +x
 # Get the filename and extension
-file="$current_dir/crack.gif"
+file="$current_dir/stop-it.gif"
 filename=$(basename "$file")
 extension="${filename##*.}"
 
